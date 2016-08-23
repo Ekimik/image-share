0806670f6d4a7a2f32e325522f69576cfff8b05c
Jan Jíša <j.jisa@seznam.cz>
2016-08-23 20:57:47 +0200

    base working example of app

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -3,8 +3,8 @@
 namespace App\Presenters;
 
 use \Nette\Application\UI\Presenter,
-    \App\Model\Services\ConfigParams,
-    \Kdyby\Facebook\Facebook;
+    \App\Model\Services\ConfigParams;
+//    \Kdyby\Facebook\Facebook;
 
 /**
  * Base presenter for all application presenters.
@@ -17,15 +17,15 @@ abstract class BasePresenter extends Presenter {
      */
     protected $cfgParams;
 
-    /**
-     *
-     * @var Facebook
-     */
-    protected $fb;
+//    /**
+//     *
+//     * @var Facebook
+//     */
+//    protected $fb;
 
-    public function injectServices(ConfigParams $cfgParams, Facebook $fb) {
+    public function injectServices(ConfigParams $cfgParams/*, Facebook $fb*/) {
         $this->cfgParams = $cfgParams;
-        $this->fb = $fb;
+//        $this->fb = $fb;
     }
 
     protected function startup() {
--- app/presenters/DefaultPresenter.php
+++ app/presenters/DefaultPresenter.php
@@ -12,27 +12,18 @@ use \App\Forms\UploadForm,
 class DefaultPresenter extends BasePresenter {
 
     public function renderDefault() {
-        $this->template->fbShareForm = $this['fbShareForm'];
+//        $this->template->fbShareForm = $this['fbShareForm'];
         $this->template->oneDriveShareForm = $this['oneDriveShareForm'];
     }
 
-    function actionFoo() {
-
-        $this->fb->api('/1736250356617235/feed', 'POST', [
-            'message' => 'Hello there fellas!',
-            'access_token' => 'EAABkZBeRE0ZA0BAN2RHYyZCc8WbhRW79qbBCxSZBFuf794zmCwwV7zaA0ZBuHc0J0M9TFA8aiUXjYUwerOpOaUic9hux1u2MCDJ20HlNKiwuRKJvQJ1018LtsLS8ZCO67ZCamd2DXDKpMzTRhLtMEjmX0PSr0ZCryjBCbqQ3vyK8UgZDZD'
-            ]);
-        exit();
-    }
-
-    protected function createComponentFbShareForm() {
-        $fc = new FormControl();
-        $fc->setTemplateFile(FORM_TEMPLATES_DIR . '/UploadForm.latte');
-        $fc->setAboutBlockName('fbShareForm');
-        $fc->setCfgParams($this->cfgParams);
-        $fc->setFormName('fbShareForm');
-        return $fc;
-    }
+//    protected function createComponentFbShareForm() {
+//        $fc = new FormControl();
+//        $fc->setTemplateFile(FORM_TEMPLATES_DIR . '/UploadForm.latte');
+//        $fc->setAboutBlockName('fbShareForm');
+//        $fc->setCfgParams($this->cfgParams);
+//        $fc->setFormName('fbShareForm');
+//        return $fc;
+//    }
 
     /**
      * @return UploadForm
